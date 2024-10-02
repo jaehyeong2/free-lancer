fabb7da2cf83c0155cfa200f2f959a98605d154f
jaehyeong2 <wogud1514@gmail.com>
2024-10-02 19:32:23 +0900

    feature: client / api 모듈에 core 모듈 Implementation

--- client-api/src/main/kotlin/jjfactory/freelancer/application/FreeLancerFacade.kt
+++ client-api/src/main/kotlin/jjfactory/freelancer/application/FreeLancerFacade.kt
@@ -1,9 +1,14 @@
 package jjfactory.freelancer.application
 
+import jjfactory.freelancer.domain.freelancer.FreeLancerCommand
+import jjfactory.freelancer.domain.freelancer.FreeLancerService
 import org.springframework.stereotype.Component
 
 @Component
 class FreeLancerFacade(
-
+    private val freeLancerService: FreeLancerService
 ) {
+    fun join(command: FreeLancerCommand.Create){
+        freeLancerService.store(command)
+    }
 }
--- company-api/src/main/kotlin/jjfactory/freelancer/application/FreeLancerFacade.kt
+++ company-api/src/main/kotlin/jjfactory/freelancer/application/FreeLancerFacade.kt
@@ -1,9 +1,10 @@
 package jjfactory.freelancer.application
 
+import jjfactory.freelancer.domain.freelancer.FreeLancerService
 import org.springframework.stereotype.Component
 
 @Component
 class FreeLancerFacade(
-
+    private val freeLancerService: FreeLancerService
 ) {
 }
