2d20efd5f730ffd906bab6b8baf9ef72084b73f0
jaehyeong2 <wogud1514@gmail.com>
2024-10-02 12:53:25 +0900

    init

---
+++ src/main/kotlin/jjfactory/freelancer/FreelancerApplication.kt
@@ -0,0 +1,11 @@
+package jjfactory.freelancer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FreelancerApplication
+
+fun main(args: Array<String>) {
+	runApplication<FreelancerApplication>(*args)
+}
